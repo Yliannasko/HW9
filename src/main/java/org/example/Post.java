ce53f495585c4a348008ad43438cc197af0b7286
Yulia <ylianna_sko@mail.ru>
2023-05-03 10:05:41 +0300

    two

---
+++ src/main/java/org/example/Post.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Post {
+    public FormDate birthday;
+    public String name;
+    public String passport;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+
+}
